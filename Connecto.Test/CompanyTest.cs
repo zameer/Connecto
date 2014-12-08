96c51376e9011df726cc2e0c103aa32ecf565964
zameer <ahamedzameer@hotmail.com>
2014-12-08 16:03:52 +0530

    clean after removing console test project

--- Connecto.Test/CompanyTest.cs
+++ Connecto.Test/CompanyTest.cs
@@ -9,11 +9,11 @@ namespace Connecto.Test
     {
         private readonly CompanyRepository _company = ConnectoFactory.CompanyRepository;
         [TestMethod]
-        public void TestMethod1()
+        public void CompanyAndLocation()
         {
             var company = _company.Get(2);
             var locations = _company.Locations(company.CompanyId);
-            Assert.AreEqual(1, locations.Count);
+            Assert.AreEqual(2, locations.Count);
         }
     }
 }
