25324c9e9eebdb347feafb56bbc0ca646da50951
Mohamed Naizar <enaizar@yahoo.co.uk>
2015-01-04 09:35:06 +0530

    Contact Business Object Added

---
+++ Connecto.BusinessObjects/Contact.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Connecto.BusinessObjects
+{
+    public class Contact : Connecto
+    {
+        public int ContactId { get; set; }
+        public Guid ContactGuid { get; set; }
+        public int EmployeeId { get; set; }
+        public string LandNumber { get; set; }
+        public string MobileNumber { get; set; }
+        public string AddressNo { get; set; }
+        public string AddressStreet { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
+    }
+}
