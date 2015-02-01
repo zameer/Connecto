3028441f9cf9425e140231c5666a2f2edf07993f
Ahamed Zameer <ahamedzameer@hotmail.com>
2015-02-01 10:53:37 +0530

    EntityPersonAdded

---
+++ Connecto.DataObjects/EntityFramework/EntityPerson.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Connecto.DataObjects.EntityFramework
+{
+    [Table("Person", Schema = "HumanResource")]
+    public class EntityPerson : EntityConnecto
+    {
+        [Key]
+        public int PersonId { get; set; }
+        public Guid PersonGuid { get; set; }
+        public string Description { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public virtual ICollection<EntityContact> Contacts { get; set; }
+    }
+}
