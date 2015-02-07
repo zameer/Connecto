2a9d049a7aa7986e8e10b8a648488796fb8d312d
Ahamed Zameer <ahamedzameer@hotmail.com>
2015-02-07 15:51:26 +0530

    menu made dynamic

---
+++ Connecto.Web/Scripts/connecto/Connectojs.js
@@ -0,0 +1,6 @@
+﻿$(function () {});
+$.fn.syncMenu = function (settings) {
+    $(this).attr('class', bindCss('ao'));
+    $.each(settings, function () { $('#menu-' + this.name).attr('class', bindCss(this.option)); });
+    function bindCss(opt) { return opt == 'a' ? 'active' : (opt == 'o' ? 'open' : (opt == 'ao' ? 'active open' : ''));};
+};
