d4ba215e979b60febe3e748c17b33f5a9f84e59c
Ahamed Zameer <ahamedzameer@hotmail.com>
2015-03-28 14:53:43 +0530

    Product base added

---
+++ Connecto.BusinessObjects/ProductBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Connecto.BusinessObjects
+{
+    public class ProductBase : Connecto
+    {
+        public int Quantity { get; set; }
+        public int QuantityActual { get; set; }
+        public int QuantityLower { get; set; }
+    }
+}
---
+++ Connecto.DataObjects/EntityFramework/EntityProductBase.cs
@@ -0,0 +1,12 @@
+﻿using Connecto.Common.Enumeration;
+using System;
+
+namespace Connecto.DataObjects.EntityFramework
+{
+    public class EntityProductBase : EntityConnecto
+    {
+        public int Quantity { get; set; }
+        public int QuantityActual { get; set; }
+        public int QuantityLower { get; set; }
+    }
+}
