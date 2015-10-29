7c7a7381fff98fd531298de60820b3d4af11bca7
Mohamed Naizar <enaizar@yahoo.co.uk>
2015-10-29 08:24:03 +0530

    some
    some chngs in sales

---
+++ Connecto.App/ModelValidator/WriteoffDetailValidator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Connecto.BusinessObjects;
+using Connecto.Repositories;
+
+namespace Connecto.App.ModelValidator{
+    public class WriteoffDetailValidator
+    {
+        private readonly WriteoffDetailCart _item;
+        private WriteoffDetailRepository _repo;
+        public WriteoffDetailValidator()
+        {
+        }
+        public WriteoffDetailValidator(WriteoffDetailRepository repo)
+        {
+            _repo = repo;
+        }
+        public WriteoffDetailValidator(WriteoffDetailCart record, WriteoffDetailRepository repo)
+        {
+            _item = record;
+            _repo = repo;
+        }
+
+        public List<ConnectoException> Validate()
+        {
+            var errors = new List<ConnectoException>();
+            if (string.IsNullOrEmpty(_item.ProductCode)) errors.Add(new ConnectoException { Message = "Product code cannot be empty." });
+            return errors;
+        }
+    }
+}
