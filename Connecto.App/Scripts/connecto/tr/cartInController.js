d75d9064d4247bee0832464598e49aa4ad26f3a9
zameer <ahamedzameer@hotmail.com>
2015-12-04 14:28:57 +0530

    cartIn-supplier-selected

--- Connecto.App/Scripts/connecto/tr/cartInController.js
+++ Connecto.App/Scripts/connecto/tr/cartInController.js
@@ -56,6 +56,7 @@ trControllers.controller(cName + 'Ctrl', ['$scope', '$filter', '$http', '$routeP
       $scope.setProduct = function () {
           if ($scope.productCodeSelected != undefined) {
               $scope.product.selected = $filter('getById')($scope.products, $scope.productCodeSelected.ProductId, "ProductId");
+              $scope.supplier.selected = $filter('getById')($scope.suppliers, $scope.productCodeSelected.SupplierId, "SupplierId");
               if ($scope.item == undefined) $scope.item = {};
               $scope.item.ProductCode = $scope.productCodeSelected.ProductCode == undefined ? $scope.productCodeSelected : $scope.productCodeSelected.ProductCode;
               $scope.filterProduct();
