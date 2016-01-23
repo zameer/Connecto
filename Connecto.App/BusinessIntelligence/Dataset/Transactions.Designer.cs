284081001a7731150ff8bbce0005a67942adbf89
Ahamed Zameer <ahamedzameer@hotmail.com>
2016-01-23 09:48:55 +0530

    invoice-by-id

--- Connecto.App/BusinessIntelligence/Dataset/Transactions.Designer.cs
+++ Connecto.App/BusinessIntelligence/Dataset/Transactions.Designer.cs
@@ -19515,17 +19515,17 @@ namespace Connecto.App.BusinessIntelligence.Dataset.TransactionsTableAdapters {
             this._commandCollection[0].CommandText = "Product.GetInvoiceById";
             this._commandCollection[0].CommandType = global::System.Data.CommandType.StoredProcedure;
             this._commandCollection[0].Parameters.Add(new global::System.Data.SqlClient.SqlParameter("@RETURN_VALUE", global::System.Data.SqlDbType.Int, 4, global::System.Data.ParameterDirection.ReturnValue, 10, 0, null, global::System.Data.DataRowVersion.Current, false, null, "", "", ""));
-            this._commandCollection[0].Parameters.Add(new global::System.Data.SqlClient.SqlParameter("@Id", global::System.Data.SqlDbType.TinyInt, 1, global::System.Data.ParameterDirection.Input, 3, 0, null, global::System.Data.DataRowVersion.Current, false, null, "", "", ""));
+            this._commandCollection[0].Parameters.Add(new global::System.Data.SqlClient.SqlParameter("@Id", global::System.Data.SqlDbType.Int, 1, global::System.Data.ParameterDirection.Input, 3, 0, null, global::System.Data.DataRowVersion.Current, false, null, "", "", ""));
         }
         
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "4.0.0.0")]
         [global::System.ComponentModel.Design.HelpKeywordAttribute("vs.data.TableAdapter")]
         [global::System.ComponentModel.DataObjectMethodAttribute(global::System.ComponentModel.DataObjectMethodType.Fill, true)]
-        public virtual int Fill(Transactions.InvoiceByIdDataTable dataTable, global::System.Nullable<byte> Id) {
+        public virtual int Fill(Transactions.InvoiceByIdDataTable dataTable, global::System.Nullable<int> Id) {
             this.Adapter.SelectCommand = this.CommandCollection[0];
             if ((Id.HasValue == true)) {
-                this.Adapter.SelectCommand.Parameters[1].Value = ((byte)(Id.Value));
+                this.Adapter.SelectCommand.Parameters[1].Value = ((int)(Id.Value));
             }
             else {
                 this.Adapter.SelectCommand.Parameters[1].Value = global::System.DBNull.Value;
@@ -19541,10 +19541,10 @@ namespace Connecto.App.BusinessIntelligence.Dataset.TransactionsTableAdapters {
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "4.0.0.0")]
         [global::System.ComponentModel.Design.HelpKeywordAttribute("vs.data.TableAdapter")]
         [global::System.ComponentModel.DataObjectMethodAttribute(global::System.ComponentModel.DataObjectMethodType.Select, true)]
-        public virtual Transactions.InvoiceByIdDataTable GetData(global::System.Nullable<byte> Id) {
+        public virtual Transactions.InvoiceByIdDataTable GetData(global::System.Nullable<int> Id) {
             this.Adapter.SelectCommand = this.CommandCollection[0];
             if ((Id.HasValue == true)) {
-                this.Adapter.SelectCommand.Parameters[1].Value = ((byte)(Id.Value));
+                this.Adapter.SelectCommand.Parameters[1].Value = ((int)(Id.Value));
             }
             else {
                 this.Adapter.SelectCommand.Parameters[1].Value = global::System.DBNull.Value;
